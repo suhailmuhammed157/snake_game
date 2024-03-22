19f5e3d44b553ee8c8128688a7c41c2c6b1d5770
SuhailMP <suhailmuhammed157@gmail.com>
2024-03-22 14:43:06 +0400

    add multiple blocks for snake and move based on logic

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import time
 
 from pygame.locals import *
 
+SIZE = 40
+
 
 class Snake:
-    def __init__(self, parent_screen):
+    def __init__(self, parent_screen, length):
         self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.length = length
+        self.x = [SIZE] * length
+        self.y = [SIZE] * length
         self.snake_direction = "right"
 
     def draw(self):
         self.parent_screen.fill((92, 25, 84))
-        self.parent_screen.blit(self.block, (self.x, self.y))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
     def move_up(self):
@@ -30,23 +34,34 @@ class Snake:
         self.snake_direction = "right"
 
     def walk(self):
+
+        # Let the blocks excluding the first block take the previous position
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
+        # Move the first block to SIZE pixels based on direction
         if self.snake_direction == "up":
-            self.y -= 10
+            self.y[0] -= SIZE
         if self.snake_direction == "down":
-            self.y += 10
+            self.y[0] += SIZE
         if self.snake_direction == "left":
-            self.x -= 10
+            self.x[0] -= SIZE
         if self.snake_direction == "right":
-            self.x += 10
+            self.x[0] += SIZE
+
+
+
+
         self.draw()
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500, 500))
+        self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((92, 25, 84))
-        self.snake = Snake(parent_screen=self.surface)
+        self.snake = Snake(parent_screen=self.surface, length=2)
         self.snake.draw()
 
     def run(self):
