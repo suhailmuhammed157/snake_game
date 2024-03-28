b81ff3feed97afdf8bcd90966e59f68415ede52e
Suhail MP <suhailmuhammed157@gmail.com>
2024-03-28 21:58:15 +0400

    add logic to show the game score

--- main.py
+++ main.py
@@ -92,9 +92,18 @@ class Game:
 
         return False
 
+    def display_score(self):
+        font = pygame.font.SysFont('arial',30)
+        score = font.render(f"Score: {self.snake.length}",True,(255,255,255))
+        self.surface.blit(score,(800,10))
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+
 
         if self.is_collision(self.apple.x, self.apple.y, self.snake.x[0], self.snake.y[0]):
             self.snake.add_block()
